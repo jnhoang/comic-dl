b834c15a1c1ad866191ecea4b9811598cbe5c1d5
jnhustin <jnhustin@gmail.com>
2019-07-14 08:42:13 -0700

    renamed class files

--- comic-package/downloader.py
+++
@@ -1,8 +0,0 @@
-import requests
-import os
-from tempfile import TemporaryDirectory
-from site_info import SiteInfo
-
-class Downloader():
-  def __init__(self):
-    self.site_info     =  SiteInfo()
